f4fc1e49c36d5cec8af085c9a9772e6f18588ea2
Elena Verkholaz <elena.verkholaz@gmail.com>
2016-11-14 17:41:06 +0200

    HW3 - task 1

---
+++ src/teask3_1/Bird.java
@@ -0,0 +1,15 @@
+package teask3_1;
+
+/**
+ * Created by elve on 11/14/2016.
+ */
+
+public class Bird {
+
+    void sign(){
+        System.out.println("I am walking");
+        System.out.println("I am flying");
+        System.out.println("I am singing");
+        System.out.println("I am Bird");
+    }
+}
---
+++ src/teask3_1/NewBird.java
@@ -0,0 +1,14 @@
+package teask3_1;
+
+/**
+ * Created by elve on 11/14/2016.
+ */
+public class NewBird {
+
+    public static void main(String[] args) {
+        Bird parrot = new Bird();
+        parrot.sign();
+    }
+
+}
+
